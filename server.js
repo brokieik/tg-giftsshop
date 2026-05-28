71534de7cbf1475adfd439d9b64caf2e3c0c59f2
brokieik <iyruslaniy@gmail.com>
2026-05-28 03:32:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ require('dotenv').config();
     });
   }
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log('✅ Server running on port ' + PORT);
     if (bot) {
